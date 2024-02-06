a72ae473f965438aaad7b0c51c0e77541fac0515
maximiloi <maximiloi@gmail.com>
2024-02-06 10:51:31 +0300

    feat: add bg-image

---
+++ src/components/App/index.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <main className="mx-auto max-w-screen-xl">
+      <h1 className="text-center text-3xl font-bold">
+        Калькулятор CDEK | Vite + React
+      </h1>
+    </main>
+  );
+};
+
+export default App;
